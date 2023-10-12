c7d99f7de9477069240514833b26c99b87085528
Justin Cordova <justcord10@gmail.com>
2023-10-11 19:55:25 -0700

    implement queue and stack classes using the pseudoclassical method in queue.js and stack.ja respectively

--- sprint-one/src/pseudoclassical/stack.js
+++ sprint-one/src/pseudoclassical/stack.js
@@ -1,6 +1,23 @@
 var Stack = function() {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
+  this.storage = {};
+  this.numKey = -1;
+};
+
+Stack.prototype.push = function(value) {
+  this.numKey ++;
+  this.storage[this.numKey] = value;
+};
+Stack.prototype.pop = function() {
+  if (this.numKey === -1) {
+    return;
+  }
+  var temp = this.storage[this.numKey];
+  delete this.storage[this.numKey];
+  this.numKey --;
+  return temp;
+};
+Stack.prototype.size = function() {
+  return this.numKey + 1;
 };
 
 
