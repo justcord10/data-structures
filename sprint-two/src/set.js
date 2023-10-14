178c678390e7ee89fc2a59e6350758c0cc78b32c
Justin Cordova <justcord10@gmail.com>
2023-10-13 20:04:18 -0700

    implemented the set data structure using the prototypal pattern in set.js

--- sprint-two/src/set.js
+++ sprint-two/src/set.js
@@ -1,19 +1,43 @@
 var Set = function() {
   var set = Object.create(setPrototype);
-  set._storage = null; // fix me
+  set._storage = [];
   return set;
 };
 
 var setPrototype = {};
 
 setPrototype.add = function(item) {
+//check if item exists in storage array
+  if (this.contains(item)) {
+    return;
+  }
+  //push it into storage array
+  this._storage.push(item);
 };
 
 setPrototype.contains = function(item) {
+  //iterate through our storage array and
+  for (let i = 0; i < this._storage.length; i++) {
+    //check if item is === to current index spot storage
+    if (item === this._storage[i]) {
+      return true;
+    }
+  }
+  return false;
 };
 
 setPrototype.remove = function(item) {
+  //iterate through the set storage array
+  for (let i = 0; i < this._storage.length; i++) {
+    //check if item is === to current index spot storage
+    if (item === this._storage[i]) {
+      //slice that mutha out
+      this._storage.splice(i, 1);
+    }
+  }
 };
+
+// make tests on contains in spec
 
 /*
  * Complexity: What is the time complexity of the above functions?
